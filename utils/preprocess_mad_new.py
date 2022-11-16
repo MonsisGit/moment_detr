7ea5e4c112bf32df49999274912eb29acb4ccbef
monsi <simon.goldhofer@gmail.com>
2022-11-16 14:29:58 +0000

    preprocessing improvements

--- utils/preprocess_mad_new.py
+++ utils/preprocess_mad_new.py
@@ -7,26 +7,31 @@ import pickle as pk
 from tqdm import tqdm
 import traceback
 
+
 class MADdataset():
 
     def __init__(self, root, video_feat_file,
-                 fps, generated_feats_save_path, log_folder, clip_length_in_seconds):
+                 generated_feats_save_path, log_folder):
 
-        self.max_words = 0
-        self.fps = fps
+        self.sampling_fps = None
+        self.sampling_mode = None
+        self.clip_length_in_frames = None
+        self.clip_length_in_seconds = None
+        self.dataset_fps = None
         self.root = root
+        self.rng = np.random.default_rng(42)
         self.generated_feats_save_path = generated_feats_save_path
         self.log_folder = log_folder
-        self.clip_length_in_seconds = clip_length_in_seconds
-        self.clip_length_in_frames = self.clip_length_in_seconds * self.fps
         # load annotation file
         self.annos = []
         self.old_annos = []
-        assert os.path.exists(video_feat_file)
+        print(f'Loading {root}/{video_feat_file}')
         self.video_feats = h5py.File(f'{root}/{video_feat_file}', 'r')
         self.discarded_data_counter = 0
 
-    def compute_annotations(self, anno_path, anno_save_path):
+    def compute_annotations(self, anno_path, anno_save_path, dataset_fps,
+                            clip_length_in_seconds, l2_normalize,
+                            process_fraction, sampling_mode, sampling_fps):
         '''
             The function processes the annotations computing language tokenizationa and query features.
             Construct the moment annotations for training and the target iou2d map.
@@ -41,12 +46,17 @@ class MADdataset():
         # compute the annotation data and dump it in a pickle file
         self.annos = []
         self.old_annos = []
+        self.clip_length_in_seconds = clip_length_in_seconds
+        self.dataset_fps = dataset_fps
+        self.clip_length_in_frames = clip_length_in_seconds * dataset_fps
         self.discarded_data_counter = 0
+        self.sampling_mode = sampling_mode
+        self.sampling_fps = sampling_fps
 
         annos = json.load(open(f'{self.root}{anno_path}', 'r'))
 
         print(f'Processing {anno_path} ..')
-        for k, anno in tqdm(annos.items()):
+        for k, anno in tqdm(list(annos.items())[0:int(len(annos.items()) * process_fraction)]):
             # Unpack Info ----------------------------------------------------------------
 
             movie = anno['movie']
@@ -58,9 +68,10 @@ class MADdataset():
             if timestamp[0] < timestamp[1]:
                 moment = [max(timestamp[0], 0), min(timestamp[1], duration)]
 
-                start = int(moment[0] * self.fps)
-                stop = int(moment[1] * self.fps)
+                start = int(moment[0] * self.dataset_fps)
+                stop = int(moment[1] * self.dataset_fps)
 
+                # frames_idx is in frame space
                 frames_idx = [start, stop]
 
                 # Save preprocessed annotations ----------------------------------------------
@@ -72,25 +83,19 @@ class MADdataset():
                     'sentence': sentence,
                     'movie_duration': duration,
                 }
-                video_features, start_moment, stop_moment = self._get_video_features(temp_dict, movie)
+                video_features, start_moment, stop_moment = self._get_video_features(temp_dict, l2_normalize)
                 dump_dict = {
                     'id': k,
                     'moment': moment,
                     'relevant_windows': [[start_moment, stop_moment]],
                     'query': sentence,
                     'duration': self.clip_length_in_seconds,
-                    "relevant_clip_ids": [i for i in
-                                          range(int(start_moment / 2), int(stop_moment / 2))],
-                    "saliency_scores": [[0, 0, 0] for _ in
-                                        range(int(start_moment / 2), int(stop_moment / 2))]
                 }
 
-                if self._check_dict(temp_dict,dump_dict):
-                    self.old_annos.append(temp_dict)
-                    self.annos.append(dump_dict)
-                    np.savez(f'{self.root}/{self.generated_feats_save_path}{k}.npz', features=video_features)
-                else:
-                    self.discarded_data_counter += 1
+                self.old_annos.append(temp_dict)
+                self.annos.append(dump_dict)
+                np.savez(f'{self.root}/{self.generated_feats_save_path}{k}.npz', features=video_features)
+
             else:
                 self.discarded_data_counter += 1
 
@@ -109,23 +114,7 @@ class MADdataset():
             f.write("\n".join([json.dumps(e) for e in self.old_annos]))
         print(f'Saved old annotations log to: {anno_save_path}')
 
-
-    def _check_dict(self, old_annos, annos, exact_timestamps=False):
-        try:
-            if not exact_timestamps:
-                assert len(old_annos["saliency_scores"]) != 0, "saliency scores are zero"
-
-            assert len(old_annos["relevant_windows"][0]) != 0
-            assert old_annos["relevant_windows"][0][0] < old_annos["relevant_windows"][0][1]
-            assert 0 <= old_annos["relevant_windows"][0][0] < old_annos["relevant_windows"][0][
-                1], f'relevant window: {old_annos["relevant_windows"][0]}\noriginal data:\n{annos}'
-            return True
-        except Exception:
-            traceback.print_exc()
-            return False
-
-
-    def _get_video_features(self, anno, movie):
+    def _get_video_features(self, anno, l2_normalize):
         '''
             INPUTS:
             anno: annotation data, contains all the preprocessed information
@@ -149,28 +138,54 @@ class MADdataset():
         start_window = max(start_idx - offset, 0)
         stop_window = start_window + self.clip_length_in_frames
 
-        if not stop_window <= anno['movie_duration'] * self.fps:
-            stop_window = int(anno['movie_duration'] * self.fps)
+        if not stop_window <= anno['movie_duration'] * self.dataset_fps:
+            stop_window = int(anno['movie_duration'] * self.dataset_fps)
             start_window = stop_window - self.clip_length_in_frames
 
-        feats = self.video_feats[movie][start_window:stop_window]
+        feats = self.video_feats[anno['movie']][start_window:stop_window]
 
         assert feats.shape[0] == self.clip_length_in_frames
 
-        # Compute moment position within the window
-        start_moment = max((start_idx - start_window) / self.fps, 0)
-        stop_moment = min((stop_idx - start_window) / self.fps, self.clip_length_in_seconds)
+        # Compute moment position within the window in seconds
+        start_moment = max((start_idx - start_window) / self.dataset_fps, 0)
+        stop_moment = min((stop_idx - start_window) / self.dataset_fps, self.clip_length_in_seconds)
 
+        if l2_normalize:
+            feats = self._l2_normalize_np_array(feats)
+
+        feats = self._sampling(feats)
         return feats, start_moment, stop_moment
+
+    def _sampling(self, feats):
+        if self.sampling_mode == 'None':
+            return feats
+        elif self.sampling_mode == 'fixed':
+            feats = feats[::int(5 / self.sampling_fps)]
+        elif self.sampling_mode == 'random':
+            num_frames = int(feats.shape[0] / (5 / self.sampling_fps))
+            feats = self.rng.choice(feats, size=num_frames, replace=False, axis=0, shuffle=False)
+        elif self.sampling_mode == 'pooling':
+            num_frames_to_pool = int(5 / self.sampling_fps)
+            first_dim = feats.shape[0] / num_frames_to_pool
+            feats = feats.reshape(int(first_dim), num_frames_to_pool, -1).mean(axis=1)
+        return feats
+
+    def _l2_normalize_np_array(self, feats, eps=1e-5):
+        """np_array: np.ndarray, (*, D), where the last dim will be normalized"""
+        return feats / (np.linalg.norm(feats, axis=-1, keepdims=True) + eps)
 
 
 if __name__ == "__main__":
     preprocessor = MADdataset(root='/nfs/data3/goldhofer/mad_dataset/',
                               video_feat_file='CLIP_L14_frames_features_5fps.h5',
                               generated_feats_save_path='clip_frame_features_transformed_exact/',
-                              log_folder='meta_log/',
-                              fps=5,
-                              clip_length_in_seconds=150)
+                              log_folder='meta_log/')
 
     preprocessor.compute_annotations(anno_path=f'annotations/MAD_val.json',
-                                     anno_save_path='annotations/MAD_val_transformed.json')
+                                     anno_save_path='annotations/MAD_val_transformed.json',
+                                     dataset_fps=5,
+                                     clip_length_in_seconds=150,
+                                     l2_normalize=False,
+                                     process_fraction=0.01,
+                                     sampling_fps=10,
+                                     sampling_mode='None')
