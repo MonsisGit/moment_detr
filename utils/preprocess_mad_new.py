14d51624d3305246222e3efa84c766e67a2d31c8
monsi <simon.goldhofer@gmail.com>
2022-11-16 13:51:54 +0000

    preprocessing script progess

--- utils/preprocess_mad_new.py
+++ utils/preprocess_mad_new.py
@@ -5,11 +5,11 @@ import random
 import numpy as np
 import pickle as pk
 from tqdm import tqdm
-
+import traceback
 
 class MADdataset():
 
-    def __init__(self, anno_file, anno_save_file, root, feat_file,
+    def __init__(self, root, video_feat_file,
                  fps, generated_feats_save_path, log_folder, clip_length_in_seconds):
 
         self.max_words = 0
@@ -19,57 +19,14 @@ class MADdataset():
         self.log_folder = log_folder
         self.clip_length_in_seconds = clip_length_in_seconds
         self.clip_length_in_frames = self.clip_length_in_seconds * self.fps
-        self.anno_save_file = anno_save_file
         # load annotation file
         self.annos = []
         self.old_annos = []
-        annos = json.load(open(f'{root}{anno_file}', 'r'))
-        self._compute_annotations(annos)
+        assert os.path.exists(video_feat_file)
+        self.video_feats = h5py.File(f'{root}/{video_feat_file}', 'r')
+        self.discarded_data_counter = 0
 
-        # Get correct data for language
-        if lang_feat_type == 'clip' and os.path.exists(lang_feat_file):
-            self.load_clip_lang_feats(lang_feat_file)
-        else:
-            raise ValueError('Select a correct type of lang feat - Glove is deprecated.')
-
-        self.movies = {a['movie']: a['movie_duration'] for a in self.annos}
-        self.feats = movie2feats(feat_file, self.movies.keys())
-
-        if self.max_words > 50:
-            self.max_words = 50
-
-    def __len__(self):
-        return len(self.annos)
-
-    def get_duration(self, idx):
-        return self.annos[idx]['movie_duration']
-
-    def get_sentence(self, idx):
-        return self.annos[idx]['sentence']
-
-    def get_moment(self, idx):
-        '''
-            return moment duration in seconds
-        '''
-        return self.annos[idx]['moment']
-
-    def get_vid(self, idx):
-        return self.annos[idx]['movie']
-
-    def get_number_of_windows(self, idx):
-        movie = self.annos[idx]['movie']
-        return len(self.windows[movie])
-
-    def load_clip_lang_feats(self, file):
-        with h5py.File(file, 'r') as f:
-            for i, anno in enumerate(self.annos):
-                lang_feat = f[anno['id']][:]
-                self.annos[i]['query'] = torch.from_numpy(lang_feat).float()
-                self.annos[i]['wordlen'] = len(lang_feat)
-
-        self.max_words = max([a['wordlen'] for a in self.annos])
-
-    def _compute_annotations(self, annos):
+    def compute_annotations(self, anno_path, anno_save_path):
         '''
             The function processes the annotations computing language tokenizationa and query features.
             Construct the moment annotations for training and the target iou2d map.
@@ -83,7 +40,12 @@ class MADdataset():
         '''
         # compute the annotation data and dump it in a pickle file
         self.annos = []
+        self.old_annos = []
+        self.discarded_data_counter = 0
 
+        annos = json.load(open(f'{self.root}{anno_path}', 'r'))
+
+        print(f'Processing {anno_path} ..')
         for k, anno in tqdm(annos.items()):
             # Unpack Info ----------------------------------------------------------------
 
@@ -123,23 +85,45 @@ class MADdataset():
                                         range(int(start_moment / 2), int(stop_moment / 2))]
                 }
 
-                self.old_annos.append(temp_dict)
-                self.annos.append(dump_dict)
-                np.savez(f'{self.root}/{self.generated_feats_save_path}{k}.npz', features=video_features)
+                if self._check_dict(temp_dict,dump_dict):
+                    self.old_annos.append(temp_dict)
+                    self.annos.append(dump_dict)
+                    np.savez(f'{self.root}/{self.generated_feats_save_path}{k}.npz', features=video_features)
+                else:
+                    self.discarded_data_counter += 1
+            else:
+                self.discarded_data_counter += 1
 
-        # save to file
-        self._save_annos()
+                # save to file
+        self._save_annos(anno_save_path)
+        print(f'Discarded {self.discarded_data_counter} / {len(list(annos.items()))} data')
 
-    def _save_annos(self):
-        anno_save_path = f'{self.root}{self.anno_save_file}'
+    def _save_annos(self, anno_path):
+        anno_save_path = f'{self.root}{anno_path}'
         with open(anno_save_path, "w") as f:
             f.write("\n".join([json.dumps(e) for e in self.annos]))
-        print(f'saved annotations to: {anno_save_path}')
+        print(f'Saved annotations to: {anno_save_path}')
 
-        anno_save_path = f'{self.root}{self.anno_save_file.split(".json")[0]}_log.json'
+        anno_save_path = f'{self.root}{anno_path.split(".json")[0]}_log.json'
         with open(anno_save_path, "w") as f:
             f.write("\n".join([json.dumps(e) for e in self.old_annos]))
-        print(f'saved old annotations log to: {anno_save_path}')
+        print(f'Saved old annotations log to: {anno_save_path}')
+
+
+    def _check_dict(self, old_annos, annos, exact_timestamps=False):
+        try:
+            if not exact_timestamps:
+                assert len(old_annos["saliency_scores"]) != 0, "saliency scores are zero"
+
+            assert len(old_annos["relevant_windows"][0]) != 0
+            assert old_annos["relevant_windows"][0][0] < old_annos["relevant_windows"][0][1]
+            assert 0 <= old_annos["relevant_windows"][0][0] < old_annos["relevant_windows"][0][
+                1], f'relevant window: {old_annos["relevant_windows"][0]}\noriginal data:\n{annos}'
+            return True
+        except Exception:
+            traceback.print_exc()
+            return False
+
 
     def _get_video_features(self, anno, movie):
         '''
@@ -148,6 +132,8 @@ class MADdataset():
             movie: movie id to select the correct features
             OUTPUTS:
             feat: movie features
+            start_moment: start moment in seconds
+            end_moment: end moment in seconds
         '''
 
         start_idx, stop_idx = anno['frames_idx']
@@ -167,7 +153,7 @@ class MADdataset():
             stop_window = int(anno['movie_duration'] * self.fps)
             start_window = stop_window - self.clip_length_in_frames
 
-        feats = self.feats[movie][start_window:stop_window]
+        feats = self.video_feats[movie][start_window:stop_window]
 
         assert feats.shape[0] == self.clip_length_in_frames
 
@@ -179,11 +165,12 @@ class MADdataset():
 
 
 if __name__ == "__main__":
-    preprocessor = MADdataset(anno_file=f'annotations/MAD_val.json',
-                              anno_save_file=f'annotations/MAD_val_t.json',
-                              root='/nfs/data3/goldhofer/mad_dataset/',
-                              feat_file='CLIP_L14_frames_features_5fps.h5',
+    preprocessor = MADdataset(root='/nfs/data3/goldhofer/mad_dataset/',
+                              video_feat_file='CLIP_L14_frames_features_5fps.h5',
                               generated_feats_save_path='clip_frame_features_transformed_exact/',
                               log_folder='meta_log/',
                               fps=5,
                               clip_length_in_seconds=150)
+
+    preprocessor.compute_annotations(anno_path=f'annotations/MAD_val.json',
+                                     anno_save_path='annotations/MAD_val_transformed.json')
